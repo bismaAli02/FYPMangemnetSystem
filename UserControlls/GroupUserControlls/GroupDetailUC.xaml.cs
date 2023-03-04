8afae3270cbd4b9554af0e1493dea3311bf7f0c5
bismaAli02 <bisma3131@gmail.com>
2023-03-04 15:52:21 +0500

    Group detailUC UI working

--- UserControlls/GroupUserControlls/GroupDetailUC.xaml.cs
+++ UserControlls/GroupUserControlls/GroupDetailUC.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CRUD_Operations;
+using FYPManagementSystem.UserControlls.ProjectsUserControlls;
+using LiveCharts.Definitions.Charts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +25,43 @@ namespace FYPManagementSystem.UserControlls.GroupUserControlls
     /// </summary>
     public partial class GroupDetailUC : UserControl
     {
-        public GroupDetailUC()
+        int groupId;
+        string projectTitle;
+        public GroupDetailUC(int groupId, string projectTitle)
         {
             InitializeComponent();
+            this.groupId = groupId;
+            this.projectTitle = projectTitle;
+        }
+
+        private void DisplayPieChart()
+        {
+            List<int> studentCount = new List<int>();
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM GroupStudent WHERE GroupId=@GroupId AND Status=3 ; SELECT COUNT(*) FROM GroupStudent WHERE GroupId=@GroupId AND Status=4", con);
+            cmd.Parameters.AddWithValue("@GroupId", groupId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                studentCount.Add(int.Parse(reader["Id"].ToString()));
+            }
+            studentCount.Add(studentCount[0]);
+            reader.Close();
+            // Create a new chart series
+            var series = new PieSeries();
+            // Set the data source for the series
+            var data = new List<KeyValuePair<string, int>>();
+            data.Add(new KeyValuePair<string, int>("Slice 1", 10));
+            data.Add(new KeyValuePair<string, int>("Slice 2", 20));
+            data.Add(new KeyValuePair<string, int>("Slice 3", 30));
+            series.DataContext = data;
+
+            // Set the value and category bindings for the series
+            series.ValueBinding = new PropertyNameDataPointBinding("Value");
+            series.CategoryBinding = new PropertyNameDataPointBinding("Label");
+
+            // Add the series to the pie chart
+            pieChart.Series.Add(series);
         }
     }
 }
